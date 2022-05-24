dbfe9faed5999668c50032b06d1e617b6881f7aa
Daniel Farias <danielfariias15@gmail.com>
2022-05-23 23:07:32 -0300

    add & config styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Login from './pages/Login';
 import Search from './pages/Search';
@@ -8,21 +9,27 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import GlobalStyles from './assets/styles/global';
+
+import defaultTheme from './assets/styles/themes/default';
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/search" component={Search} />
-          <Route path="/album/:id" component={Album} />
-          <Route path="/favorites" component={Favorites} />
-          <Route path="/profile/edit" component={ProfileEdit} />
-          <Route path="/profile" component={Profile} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/search" component={Search} />
+            <Route path="/album/:id" component={Album} />
+            <Route path="/favorites" component={Favorites} />
+            <Route path="/profile/edit" component={ProfileEdit} />
+            <Route path="/profile" component={Profile} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     );
   }
 }
